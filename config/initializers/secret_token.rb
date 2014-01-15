3ee39e9cd45a4d464712a19bdf05357f2ce0e6b8
Sam Walton <sam@foofrancine.com>
2014-01-15 14:51:27 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nesty::Application.config.secret_token = '06193872e0318512fd87def3840b91a26ba3b72d830fc09d717553eda76af73a7c9a77aa68c3768eff2bf04c16bd226153eb4fd68ca703dd01411b2bff3b8f5e'
